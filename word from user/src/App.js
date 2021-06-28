9a6d64c7bb4224208067fa00f25c52e447f2ece3
Sur Shri <61155043+sur2000@users.noreply.github.com>
2021-06-28 08:51:28 +0530

    Add files via upload

---
+++ word from user/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import img1 from'./assets/Group.svg'; 
+
+
+const App = () =>  {
+  return (
+   <React.Fragment>
+    <img src={img1} class="" /> 
+   <p> jehqdwouiebpyriu8o[qw2oeir[nbvqpnwioerbieurybyequowerjhEQ</p> 
+   
+     
+   </React.Fragment>
+  );
+}
+
+export default App;
